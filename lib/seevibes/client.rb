a4431caad13cf266b951017b69feddba5221532b
François Beausoleil <francois@teksol.info>
2013-10-20 14:17:27 -0400

    Commenting for illustrative purposes

--- lib/seevibes/client.rb
+++ lib/seevibes/client.rb
@@ -8,18 +8,18 @@ module Seevibes
       "Well! Hello there, #{params[:name]}"
     end
 
-    before do
-      @redis = Redis.new(url: ENV["REDIS_URL"] || raise("Missing REDIS_URL environment variable - can't continue"))
-    end
+    # before do
+    #   @redis = Redis.new(url: ENV["REDIS_URL"] || raise("Missing REDIS_URL environment variable - can't continue"))
+    # end
 
-    attr_reader :redis
+    # attr_reader :redis
 
-    get "/remember" do
-      count = redis.incr params[:name]
-      count.times.map do |index|
-        "#{params[:name]} was seen #{index + 1}!"
-      end.join("<br>")
-    end
+    # get "/remember" do
+    #   count = redis.incr params[:name]
+    #   count.times.map do |index|
+    #     "#{params[:name]} was seen #{index + 1} time#{index.nonzero? "s" : ""}!"
+    #   end.join("<br>")
+    # end
 
   end
 end
